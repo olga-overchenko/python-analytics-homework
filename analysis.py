120b8c9cad256ba7eab375a32ae434a8137e852d
Olga Overchenko <timolya24@gmail.com>
2026-01-16 23:26:43 +0200

    temporary title change

--- analysis.py
+++ analysis.py
@@ -3,6 +3,6 @@ import pandas as pd
 data = {"city": ["Kyiv", "Lviv", "Odesa"], "sales": [1200, 950, 500]}
 df = pd.DataFrame(data)
 
-print("Продажі по містах (тимчасова версія):")
+print("Продажі по містах (ТИМЧАСОВА ВЕРСІЯ !!!):")
 print(df)
 print("Середнє значення:", df["sales"].mean())
