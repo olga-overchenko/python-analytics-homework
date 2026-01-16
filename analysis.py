97cbaac088a0706d069afafee5094fe761a23a6c
Olga Overchenko <timolya24@gmail.com>
2026-01-17 00:37:09 +0200

    add average sales comment

--- analysis.py
+++ analysis.py
@@ -5,4 +5,10 @@ df = pd.DataFrame(data)
 
 print("Продажі по містах (ТИМЧАСОВА ВЕРСІЯ !!!):")
 print(df)
-print("Середнє значення:", df["sales"].mean())
+average_sales = df["sales"].mean()
+print("Середне значення:",average_sales)
+print("Це середній рівень продажів по трьох містах")
+
+
+
+
